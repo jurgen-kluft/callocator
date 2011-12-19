0d7580b4fb2cad6d7ea725f0cadf1e37c607bcb9
jurgen <jurgen@virtuos.com.cn>
2011-12-19 15:05:52 +0800

    +: added cpp file for high performance heap allocator for external memory +: added design doc for the high performance heap allocator for external memory *: rename eb allocator *: added some text to x_allocator_pool.cpp for adding the ability to free blocks

--- source/main/include/xallocator/x_allocator.h
+++ source/main/include/xallocator/x_allocator.h
@@ -22,10 +22,21 @@ namespace xcore
 	extern x_iallocator*		gCreateDlAllocator(void* mem, u32 memsize);
 	extern x_iallocator*		gCreateTlsfAllocator(void* mem, u32 memsize);
 
+	/// Allocators that manage external memory like video or sound card memory are
+	/// required to implement the following interface.
+	class x_imemory
+	{
+	public:
+		virtual const char*		name() const = 0;								///< The name of the external memory (e.g. 'PS3 Graphics Memory')
+		virtual u32				mem_range(void*& low, void*& high) const = 0;	///< The external memory address starts at @low and ends at @high. Returns the size (@high - @low)			
+		virtual void			mem_copy(void* dst, void const* src, u32 size) const = 0;
+		virtual u32				page_size() const = 0;							///< The page size of the external memory
+		virtual u32				align_size() const = 0;							///< The minimum alignment of allocated memory
+	};
+
 	/// External block allocator, manages external memory like Texture Memory, Sound Memory
 	/// by putting bookkeeping data in separate memory.
-	typedef void (*xextmem_copy)(void const* src, u32 src_size, void* dst, u32 dst_size);
-	extern x_iallocator*		gCreateEbAllocator(void* mem, u32 memsize, x_iallocator *allocator, xextmem_copy extmem_copy);
+	extern x_iallocator*		gCreateExtBlockAllocator(x_iallocator *allocator, x_imemory* memory);
 	
 	/// The forward ring allocator is a specialized allocator. You can use it when you are allocating different size blocks that
 	/// all have a life-time that doesn't differ much. This allocator is very fast in allocation O(1), deallocations effectively
