d0dbe48165a556e3159c512509a62fe7ae78b623
jurgen <jurgen@virtuos.com.cn>
2012-01-17 18:34:15 +0800

    *: refactored the pool allocator to be able to grow and shrink. By using a    tree to manage the blocks we are now allocating and deallocating from a    current block. We also know when a block is full or empty. Their are 2    type of blocks, static and dynamic, static blocks are those which are    created upon initialization, the dynamic blocks are those ones which are    added by 'growing'. With this information now available we can add parameters    for the user to specify 'shrinking' behavior.    Also adjusted the unittest to the behavior of the allocator.

--- source/test/cpp/test_x_allocator_pool.cpp
+++ source/test/cpp/test_x_allocator_pool.cpp
@@ -14,50 +14,127 @@ UNITTEST_SUITE_BEGIN(x_allocator_pool)
 {
     UNITTEST_FIXTURE(main)
     {
-		xpool_params gParams;	
-
-		x_iallocator*	gCustomAllocator;
-
         UNITTEST_FIXTURE_SETUP()
 		{
-			gParams.set_elem_size(12);
-			gParams.set_elem_alignment(16);
-			gParams.set_block_size(16);
-			gParams.set_block_initial_count(1);
-			gParams.set_block_growth_count(1);
-			gParams.set_block_max_count(2);
 
-			gCustomAllocator = gCreatePoolAllocator(gSystemAllocator, gParams);
 		}
 
         UNITTEST_FIXTURE_TEARDOWN()
 		{
-			gCustomAllocator->release();
 		}
 
-        UNITTEST_TEST(alloc3_free3)
+		UNITTEST_TEST(static_block_allocations_deallocations)
+		{
+			xpool_params params;	
+			params.set_elem_size(12);
+			params.set_elem_alignment(16);
+			params.set_block_size(16);
+			params.set_block_initial_count(4);
+			params.set_block_growth_count(1);
+			params.set_block_max_count(4);
+
+			x_iallocator* pool_allocator = gCreatePoolAllocator(gSystemAllocator, params);
+
+			const s32 N = 4*16;
+			void* allocs[N];
+
+			for (s32 j=0; j<300; j++)
+			{
+				for (s32 i=0; i<N; ++i)
+				{
+					void* mem = pool_allocator->allocate(12, params.get_elem_alignment());
+					CHECK_TRUE(x_intu::isAligned((u32)mem, params.get_elem_alignment()));
+					if (j==0)
+					{
+						CHECK_NOT_NULL(mem);
+						allocs[i] = mem;
+					}
+					else
+					{
+						CHECK_NOT_NULL(mem);
+						CHECK_EQUAL(allocs[i], mem);
+					}
+				}
+
+				// Deallocate in reversed order so that the state of the allocator will
+				// be the same as when we started to allocate and when not using
+				// dynamic added blocks
+				for (s32 i=N-1; i>=0; --i)
+				{
+					void* mem = allocs[i];
+					pool_allocator->deallocate(mem);
+				}
+			}
+			pool_allocator->release();
+		}
+
+        UNITTEST_TEST(dynamic_block_allocations_deallocations)
         {
-			void* mem1 = gCustomAllocator->allocate(12, gParams.get_elem_alignment());
-			void* mem2 = gCustomAllocator->allocate(10, gParams.get_elem_alignment());
-			void* mem3 = gCustomAllocator->allocate( 8, gParams.get_elem_alignment());
+			xpool_params params;	
+			params.set_elem_size(12);
+			params.set_elem_alignment(16);
+			params.set_block_size(16);
+			params.set_block_initial_count(4);
+			params.set_block_growth_count(1);
+			params.set_block_max_count(2048 / 16);
 
-			CHECK_TRUE(x_intu::isAligned((u32)mem1, gParams.get_elem_alignment()));
-			CHECK_TRUE(x_intu::isAligned((u32)mem2, gParams.get_elem_alignment()));
-			CHECK_TRUE(x_intu::isAligned((u32)mem3, gParams.get_elem_alignment()));
+			x_iallocator* pool_allocator = gCreatePoolAllocator(gSystemAllocator, params);
 
-			gCustomAllocator->deallocate(mem2);
-			gCustomAllocator->deallocate(mem1);
-			gCustomAllocator->deallocate(mem3);
+			const s32 N = 2048;
+			void* allocs[N];
+
+			for (s32 j=0; j<30; j++)
+			{
+				for (s32 i=0; i<N; ++i)
+				{
+					void* mem = pool_allocator->allocate(12, params.get_elem_alignment());
+					CHECK_TRUE(x_intu::isAligned((u32)mem, params.get_elem_alignment()));
+					if (j==0 || j==1)
+					{
+						CHECK_NOT_NULL(mem);
+						allocs[i] = mem;
+					}
+					else
+					{
+						// After 1 full round all dynamic blocks are there, so the behavior
+						// now will be that blocks are popped in order. This cannot be
+						// guaranteed when the allocator is still growing.
+						CHECK_NOT_NULL(mem);
+						CHECK_EQUAL(allocs[i], mem);
+					}
+				}
+	
+				// Deallocate in reversed order so that the state of the allocator will
+				// be the same as when we started to allocate.
+				for (s32 i=N-1; i>=0; --i)
+				{
+					void* mem = allocs[i];
+					pool_allocator->deallocate(mem);
+				}
+			}
+			pool_allocator->release();
         }
 
         UNITTEST_TEST(alloc_realloc_free)
         {
-			void* mem = gCustomAllocator->allocate(12, gParams.get_elem_alignment());
-			mem = gCustomAllocator->reallocate(mem, 10, gParams.get_elem_alignment());
-			mem = gCustomAllocator->reallocate(mem, 8, gParams.get_elem_alignment());
-			mem = gCustomAllocator->reallocate(mem, 4, gParams.get_elem_alignment());
-			gCustomAllocator->deallocate(mem);
-        }
+			xpool_params params;	
+			params.set_elem_size(12);
+			params.set_elem_alignment(16);
+			params.set_block_size(16);
+			params.set_block_initial_count(4);
+			params.set_block_growth_count(1);
+			params.set_block_max_count(4);
+
+			x_iallocator* pool_allocator = gCreatePoolAllocator(gSystemAllocator, params);
+
+			void* mem = pool_allocator->allocate(12, params.get_elem_alignment());
+			mem = pool_allocator->reallocate(mem, 10, params.get_elem_alignment());
+			mem = pool_allocator->reallocate(mem, 8, params.get_elem_alignment());
+			mem = pool_allocator->reallocate(mem, 4, params.get_elem_alignment());
+			pool_allocator->deallocate(mem);
+
+			pool_allocator->release();
+}
 
 	}
 }
