fe43a1ef6a058e51ea26b08a332920c63248d7e9
Jurgen Kluft <jurgen@virtuosgames.com>
2019-09-20 18:03:01 +0800

    * pool -> fsa allocator

--- source/main/include/xallocator/x_allocator_pool.h
+++ source/main/include/xallocator/x_allocator_pool.h
@@ -1,50 +1,47 @@
 #ifndef __X_POOL_ALLOCATOR_H__
 #define __X_POOL_ALLOCATOR_H__
 #include "xbase/x_target.h"
-#ifdef USE_PRAGMA_ONCE 
-#pragma once 
+#ifdef USE_PRAGMA_ONCE
+#pragma once
 #endif
-
-#include "xallocator/x_fsalloc.h"
 
 namespace xcore
 {
-	/// Forward declares
-	class xalloc;
+    /// Forward declares
+    class xalloc;
+    class xfsalloc;
 
-	struct xfsa_params
-	{
-		xfsa_params()
-			: mElemSize(4)												// The size of the element
-			, mElemAlignment(4)											// Alignment of the element
-			, mMinNumberOfBlocks(1)										// Minimum number of blocks
-			, mMaxNumberOfBlocks(256)									// Maximum number of blocks
-			, mBlockAllocator(nullptr)									// Allocator to obtain blocks from
+    struct xpool
+    {
+        xpool()
+            : m_sizeof_element(sizeof(void*))
+			, m_alignof_element(sizeof(void*))
+			, m_minimum_numberof_blocks(1)
+			, m_maximum_numberof_blocks(256)
+			, m_block_allocator(nullptr) {}
 
-		void		set_elem_size(u32 size);
-		void		set_elem_alignment(u32 alignment);
+		xfsalloc* create(xalloc * main_allocator);
 
-		void		set_block_min_count(u32 min_num_blocks);
-		void		set_block_max_count(u32 max_num_blocks);
-		void		set_block_allocator(xfsalloc* block_allocator);
+        void set_elem_size(u32 size);
+        void set_elem_alignment(u32 alignment);
+        void set_block_min_count(u32 min_num_blocks);
+        void set_block_max_count(u32 max_num_blocks);
+        void set_block_allocator(xalloc* block_allocator);
 
-		u32			get_elem_size() const;
-		u32			get_elem_alignment() const;
-		u32			get_block_min_count() const;
-		u32			get_block_max_count() const;
-		xfsalloc*	get_block_allocator() const;
+        u32       get_elem_size() const;
+        u32       get_elem_alignment() const;
+        u32       get_block_min_count() const;
+        u32       get_block_max_count() const;
+        xfsalloc* get_block_allocator() const;
 
-	private:
-		u32			mElemSize;
-		u32			mElemAlignment;
-		u32			mMinNumberOfBlocks;		
-		u32			mMaxNumberOfBlocks;
-		xfsalloc*	mBlockAllocator;
-	};
+    private:
+        u32       m_sizeof_element;
+        u32       m_alignof_element;
+        u32       m_minimum_numberof_blocks;
+        u32       m_maximum_numberof_blocks;
+        xalloc*   m_block_allocator;
+    };
 
-	extern xfsalloc*	gCreateFixedSizeAllocator(xalloc* main_allocator, xfsa_params const& params);
-	
-};
+}; // namespace xcore
 
-#endif	/// __X_POOL_ALLOCATOR_H__
-
+#endif /// __X_POOL_ALLOCATOR_H__
