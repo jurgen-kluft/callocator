6ecb22b34c3b71a2740976a43383e35eaf96f616
Jurgen Kluft <jurgen.kluft@gmail.com>
2022-10-14 21:37:16 +0800

    * package; fix typo

--- package/package.go
+++ package/package.go
@@ -3,7 +3,6 @@ package callocator
 import (
 	cbase "github.com/jurgen-kluft/cbase/package"
 	"github.com/jurgen-kluft/ccode/denv"
-	centry "github.com/jurgen-kluft/centry/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
 )
 
@@ -11,13 +10,12 @@ import (
 func GetPackage() *denv.Package {
 	// Dependencies
 	cunittestpkg := cunittest.GetPackage()
-	centrypkg := centry.GetPackage()
 	cbasepkg := cbase.GetPackage()
 
 	// The main (callocator) package
 	mainpkg := denv.NewPackage("callocator")
 	mainpkg.AddPackage(cunittestpkg)
-	mainpkg.AddPackage(cbasepkgg)
+	mainpkg.AddPackage(cbasepkg)
 	mainpkg.AddPackage(cbasepkg)
 
 	// 'callocator' library
@@ -27,7 +25,7 @@ func GetPackage() *denv.Package {
 	// 'callocator' unittest project
 	maintest := denv.SetupDefaultCppTestProject("callocator_test", "github.com\\jurgen-kluft\\callocator")
 	maintest.Dependencies = append(maintest.Dependencies, cunittestpkg.GetMainLib())
-	maintest.Dependencies = append(maintest.Dependencies, cbasepkgg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
