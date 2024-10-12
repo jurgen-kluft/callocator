b5208a9bfc27602eb8a8f44700f1dfacd70cac5c
jurgen.kluft <jurgen.kluft@gmail.com>
2024-10-12 12:19:33 +0800

    * callocator
    * expose tlsf_alloc_t as a class that can be derived from (v_resize), so
    that a virtual memory version can easily be implemented.

--- source/main/cpp/c_allocator_tlsf.cpp
+++ source/main/cpp/c_allocator_tlsf.cpp
@@ -15,49 +15,42 @@ namespace ncore
         typedef u32    uint32_t;
         typedef u64    uint64_t;
 
-#define NULL nullptr
-
-#define _TLSF_SL_COUNT 16
-#if __SIZE_WIDTH__ == 64
-#    define _TLSF_FL_COUNT 32
-#    define _TLSF_FL_MAX 38
-#else
-#    define _TLSF_FL_COUNT 25
-#    define _TLSF_FL_MAX 30
-#endif
-#define TLSF_MAX_SIZE (((size_t)1 << (_TLSF_FL_MAX - 1)) - sizeof(size_t))
-
-        class tlsf_t
+        struct tlsf_block_t
         {
-        public:
-            uint32_t           fl, sl[_TLSF_FL_COUNT];
-            struct tlsf_block* block[_TLSF_FL_COUNT][_TLSF_SL_COUNT];
-            size_t             size;
+            /* Points to the previous block.
+             * This field is only valid if the previous block is free and is actually
+             * stored at the end of the previous block.
+             */
+            tlsf_block_t* prev;
 
-            void  setup();
-            void* aalloc(size_t, size_t);
-            void* malloc(size_t size);
-            void* realloc(void*, size_t);
-            void  free(void*);
+            /* Size and block bits */
+            u64 header;
 
-#ifdef TLSF_ENABLE_CHECK
-            const char* check();
-#else
-            const char* check() { return nullptr; }
-#endif
-
-            virtual void* v_resize(size_t) = 0;
+            /* Next and previous free blocks.
+             * These fields are only valid if the corresponding block is free.
+             */
+            tlsf_block_t* next_free;
+            tlsf_block_t* prev_free;
         };
 
-        void tlsf_t::setup()
+#define NULL nullptr
+
+#define TLSF_MAX_SIZE (((size_t)1 << (TLSF_FL_MAX - 1)) - sizeof(size_t))
+
+        void* tlsf_aalloc(tlsf_alloc_t* t, size_t, size_t);
+        void* tlsf_malloc(tlsf_alloc_t* t, size_t size);
+        void* tlsf_realloc(tlsf_alloc_t* t, void*, size_t);
+        void  tlsf_free(tlsf_alloc_t* t, void*);
+
+        void tlsf_setup(tlsf_alloc_t* t)
         {
-            fl   = 0;
-            size = 0;
-            for (uint32_t i = 0; i < _TLSF_FL_COUNT; ++i)
+            t->fl   = 0;
+            t->size = 0;
+            for (uint32_t i = 0; i < TLSF_FL_COUNT; ++i)
             {
-                sl[i] = 0;
-                for (uint32_t j = 0; j < _TLSF_SL_COUNT; ++j)
-                    block[i][j] = NULL;
+                t->sl[i] = 0;
+                for (uint32_t j = 0; j < TLSF_SL_COUNT; ++j)
+                    t->block[i][j] = NULL;
             }
         }
 
@@ -76,7 +69,7 @@ namespace ncore
 /* First level (FL) and second level (SL) counts */
 #define SL_SHIFT 4
 #define SL_COUNT (1U << SL_SHIFT)
-#define FL_MAX _TLSF_FL_MAX
+#define FL_MAX TLSF_FL_MAX
 #define FL_SHIFT (SL_SHIFT + ALIGN_SHIFT)
 #define FL_COUNT (FL_MAX - FL_SHIFT + 1)
 
@@ -99,23 +92,6 @@ namespace ncore
 #    define INLINE static inline __attribute__((always_inline))
 #endif
 
-        typedef struct tlsf_block
-        {
-            /* Points to the previous block.
-             * This field is only valid if the previous block is free and is actually
-             * stored at the end of the previous block.
-             */
-            struct tlsf_block* prev;
-
-            /* Size and block bits */
-            size_t header;
-
-            /* Next and previous free blocks.
-             * These fields are only valid if the corresponding block is free.
-             */
-            struct tlsf_block *next_free, *prev_free;
-        } tlsf_block_t;
-
         // _Static_assert(sizeof(size_t) == 4 || sizeof(size_t) == 8, "size_t must be 32 or 64 bit");
         // _Static_assert(sizeof(size_t) == sizeof(void*), "size_t must equal pointer size");
         // _Static_assert(ALIGN_SIZE == BLOCK_SIZE_SMALL / SL_COUNT, "sizes are not properly set");
@@ -123,8 +99,8 @@ namespace ncore
         // _Static_assert(BLOCK_SIZE_MAX == TLSF_MAX_SIZE + BLOCK_OVERHEAD, "max allocation size is wrong");
         // _Static_assert(FL_COUNT <= 32, "index too large");
         // _Static_assert(SL_COUNT <= 32, "index too large");
-        // _Static_assert(FL_COUNT == _TLSF_FL_COUNT, "invalid level configuration");
-        // _Static_assert(SL_COUNT == _TLSF_SL_COUNT, "invalid level configuration");
+        // _Static_assert(FL_COUNT == TLSF_FL_COUNT, "invalid level configuration");
+        // _Static_assert(SL_COUNT == TLSF_SL_COUNT, "invalid level configuration");
 
         INLINE uint32_t bitmap_ffs(uint32_t x)
         {
@@ -241,7 +217,7 @@ namespace ncore
             ASSERTS(*sl < SL_COUNT, "wrong second level");
         }
 
-        INLINE tlsf_block_t* block_find_suitable(tlsf_t* t, uint32_t* fl, uint32_t* sl)
+        INLINE tlsf_block_t* block_find_suitable(tlsf_alloc_t* t, uint32_t* fl, uint32_t* sl)
         {
             ASSERTS(*fl < FL_COUNT, "wrong first level");
             ASSERTS(*sl < SL_COUNT, "wrong second level");
@@ -271,7 +247,7 @@ namespace ncore
         }
 
         /* Remove a free block from the free list. */
-        INLINE void remove_free_block(tlsf_t* t, tlsf_block_t* block, uint32_t fl, uint32_t sl)
+        INLINE void remove_free_block(tlsf_alloc_t* t, tlsf_block_t* block, uint32_t fl, uint32_t sl)
         {
             ASSERTS(fl < FL_COUNT, "wrong first level");
             ASSERTS(sl < SL_COUNT, "wrong second level");
@@ -301,7 +277,7 @@ namespace ncore
         }
 
         /* Insert a free block into the free block list and mark the bitmaps. */
-        INLINE void insert_free_block(tlsf_t* t, tlsf_block_t* block, uint32_t fl, uint32_t sl)
+        INLINE void insert_free_block(tlsf_alloc_t* t, tlsf_block_t* block, uint32_t fl, uint32_t sl)
         {
             tlsf_block_t* current = t->block[fl][sl];
             ASSERTS(block, "cannot insert a null entry into the free list");
@@ -315,7 +291,7 @@ namespace ncore
         }
 
         /* Remove a given block from the free list. */
-        INLINE void block_remove(tlsf_t* t, tlsf_block_t* block)
+        INLINE void block_remove(tlsf_alloc_t* t, tlsf_block_t* block)
         {
             uint32_t fl, sl;
             mapping(block_size(block), &fl, &sl);
@@ -323,7 +299,7 @@ namespace ncore
         }
 
         /* Insert a given block into the free list. */
-        INLINE void block_insert(tlsf_t* t, tlsf_block_t* block)
+        INLINE void block_insert(tlsf_alloc_t* t, tlsf_block_t* block)
         {
             uint32_t fl, sl;
             mapping(block_size(block), &fl, &sl);
@@ -355,7 +331,7 @@ namespace ncore
         }
 
         /* Merge a just-freed block with an adjacent previous free block. */
-        INLINE tlsf_block_t* block_merge_prev(tlsf_t* t, tlsf_block_t* block)
+        INLINE tlsf_block_t* block_merge_prev(tlsf_alloc_t* t, tlsf_block_t* block)
         {
             if (block_is_prev_free(block))
             {
@@ -369,7 +345,7 @@ namespace ncore
         }
 
         /* Merge a just-freed block with an adjacent free block. */
-        INLINE tlsf_block_t* block_merge_next(tlsf_t* t, tlsf_block_t* block)
+        INLINE tlsf_block_t* block_merge_next(tlsf_alloc_t* t, tlsf_block_t* block)
         {
             tlsf_block_t* next = block_next(block);
             ASSERTS(next, "next block can't be null");
@@ -383,7 +359,7 @@ namespace ncore
         }
 
         /* Trim any trailing block space off the end of a block, return to pool. */
-        INLINE void block_rtrim_free(tlsf_t* t, tlsf_block_t* block, size_t size)
+        INLINE void block_rtrim_free(tlsf_alloc_t* t, tlsf_block_t* block, size_t size)
         {
             ASSERTS(block_is_free(block), "block must be free");
             if (!block_can_split(block, size))
@@ -395,7 +371,7 @@ namespace ncore
         }
 
         /* Trim any trailing block space off the end of a used block, return to pool. */
-        INLINE void block_rtrim_used(tlsf_t* t, tlsf_block_t* block, size_t size)
+        INLINE void block_rtrim_used(tlsf_alloc_t* t, tlsf_block_t* block, size_t size)
         {
             ASSERTS(!block_is_free(block), "block must be used");
             if (!block_can_split(block, size))
@@ -406,7 +382,7 @@ namespace ncore
             block_insert(t, rest);
         }
 
-        INLINE tlsf_block_t* block_ltrim_free(tlsf_t* t, tlsf_block_t* block, size_t size)
+        INLINE tlsf_block_t* block_ltrim_free(tlsf_alloc_t* t, tlsf_block_t* block, size_t size)
         {
             ASSERTS(block_is_free(block), "block must be free");
             ASSERTS(block_can_split(block, size), "block is too small");
@@ -417,7 +393,7 @@ namespace ncore
             return rest;
         }
 
-        INLINE void* block_use(tlsf_t* t, tlsf_block_t* block, size_t size)
+        INLINE void* block_use(tlsf_alloc_t* t, tlsf_block_t* block, size_t size)
         {
             block_rtrim_free(t, block, size);
             block_set_free(block, false);
@@ -431,7 +407,7 @@ namespace ncore
             ASSERTS(!block_is_free(block), "sentinel block should not be free");
         }
 
-        static bool arena_grow(tlsf_t* t, size_t size)
+        static bool arena_grow(tlsf_alloc_t* t, size_t size)
         {
             size_t req_size = (t->size ? t->size + BLOCK_OVERHEAD : 2 * BLOCK_OVERHEAD) + size;
             void*  addr     = t->v_resize(req_size);
@@ -452,7 +428,7 @@ namespace ncore
             return true;
         }
 
-        static void arena_shrink(tlsf_t* t, tlsf_block_t* block)
+        static void arena_shrink(tlsf_alloc_t* t, tlsf_block_t* block)
         {
             check_sentinel(block_next(block));
             size_t size = block_size(block);
@@ -468,7 +444,7 @@ namespace ncore
             }
         }
 
-        INLINE tlsf_block_t* block_find_free(tlsf_t* t, size_t size)
+        INLINE tlsf_block_t* block_find_free(tlsf_alloc_t* t, size_t size)
         {
             size_t   rounded = round_block_size(size);
             uint32_t fl, sl;
@@ -486,27 +462,26 @@ namespace ncore
             return block;
         }
 
-        void* tlsf_t::malloc(size_t size)
+        void* tlsf_malloc(tlsf_alloc_t* t, size_t size)
         {
             size = adjust_size(size, ALIGN_SIZE);
             if (UNLIKELY(size > TLSF_MAX_SIZE))
                 return NULL;
-            tlsf_block_t* block = block_find_free(this, size);
+            tlsf_block_t* block = block_find_free(t, size);
             if (UNLIKELY(!block))
                 return NULL;
-            return block_use(this, block, size);
+            return block_use(t, block, size);
         }
 
-        void* tlsf_t::aalloc(size_t align, size_t size)
+        void* tlsf_aalloc(tlsf_alloc_t* t, size_t align, size_t size)
         {
-            tlsf_t* t      = this;
-            size_t  adjust = adjust_size(size, ALIGN_SIZE);
+            size_t adjust = adjust_size(size, ALIGN_SIZE);
 
             if (UNLIKELY(!size || ((align | size) & (align - 1)) /* align!=2**x, size!=n*align */ || adjust > TLSF_MAX_SIZE - align - sizeof(tlsf_block_t) /* size is too large */))
                 return NULL;
 
             if (align <= ALIGN_SIZE)
-                return t->malloc(size);
+                return tlsf_malloc(t, size);
 
             size_t        asize = adjust_size(adjust + align - 1 + sizeof(tlsf_block_t), align);
             tlsf_block_t* block = block_find_free(t, asize);
@@ -518,7 +493,7 @@ namespace ncore
             return block_use(t, block, adjust);
         }
 
-        void tlsf_t::free(void* mem)
+        void tlsf_free(tlsf_alloc_t* t, void* mem)
         {
             if (UNLIKELY(!mem))
                 return;
@@ -527,29 +502,27 @@ namespace ncore
             ASSERTS(!block_is_free(block), "block already marked as free");
 
             block_set_free(block, true);
-            block = block_merge_prev(this, block);
-            block = block_merge_next(this, block);
+            block = block_merge_prev(t, block);
+            block = block_merge_next(t, block);
 
             if (UNLIKELY(!block_size(block_next(block))))
-                arena_shrink(this, block);
+                arena_shrink(t, block);
             else
-                block_insert(this, block);
+                block_insert(t, block);
         }
 
-        void* tlsf_t::realloc(void* mem, size_t size)
+        void* tlsf_realloc(tlsf_alloc_t* t, void* mem, size_t size)
         {
-            tlsf_t* t = this;
-
             /* Zero-size requests are treated as free. */
             if (UNLIKELY(mem && !size))
             {
-                free(mem);
+                tlsf_free(t, mem);
                 return NULL;
             }
 
             /* Null-pointer requests are treated as malloc. */
             if (UNLIKELY(!mem))
-                return malloc(size);
+                return tlsf_malloc(t, size);
 
             tlsf_block_t* block = block_from_payload(mem);
             size_t        avail = block_size(block);
@@ -566,11 +539,11 @@ namespace ncore
                 tlsf_block_t* next = block_next(block);
                 if (!block_is_free(next) || size > avail + block_size(next) + BLOCK_OVERHEAD)
                 {
-                    void* dst = malloc(size);
+                    void* dst = tlsf_malloc(t, size);
                     if (dst)
                     {
                         nmem::memcpy(dst, mem, avail);
-                        free(mem);
+                        tlsf_free(t, mem);
                     }
                     return dst;
                 }
@@ -585,9 +558,9 @@ namespace ncore
         }
 
 #ifdef TLSF_ENABLE_CHECK
-        const char* tlsf_t::check()
+        const char* tlsf_alloc_t::check()
         {
-            tlsf_t const* t = this;
+            tlsf_alloc_t const* t = this;
             for (uint32_t i = 0; i < FL_COUNT; ++i)
             {
                 for (uint32_t j = 0; j < SL_COUNT; ++j)
@@ -638,48 +611,67 @@ namespace ncore
 #endif
     } // namespace ntlsf
 
-    class tlsf_alloc_t : public alloc_t, public ntlsf::tlsf_t
+    // ----------------------------------------------------------------------------
+    // tlsf_alloc_t
+    // ----------------------------------------------------------------------------
+
+    tlsf_alloc_t::tlsf_alloc_t() { ntlsf::tlsf_setup(this); }
+    tlsf_alloc_t::~tlsf_alloc_t() {}
+
+    void* tlsf_alloc_t::v_allocate(u32 size, u32 alignment)
+    {
+        if (alignment < 16)
+            return ntlsf::tlsf_malloc(this, size);
+        return ntlsf::tlsf_aalloc(this, alignment, size);
+    }
+
+    void tlsf_alloc_t::v_deallocate(void* ptr) { ntlsf::tlsf_free(this, ptr); }
+
+    bool tlsf_alloc_t::v_check(const char*& error_description) const
+    {
+#ifdef TLSF_ENABLE_CHECK
+        error_description = ntlsf::tlsf_alloc_t::check();
+        return !error_description;
+#else
+        error_description = nullptr;
+        return true;
+#endif
+    }
+
+    // ----------------------------------------------------------------------------
+    // tlsf_alloc_simple_t
+    // ----------------------------------------------------------------------------
+
+    class tlsf_alloc_simple_t : public tlsf_alloc_t
     {
     public:
         void* mMemory;
-        int_t mMemorySize;
+        u64   mMemorySize;
 
-        tlsf_alloc_t();
-        virtual ~tlsf_alloc_t();
+        tlsf_alloc_simple_t();
+        virtual ~tlsf_alloc_simple_t();
 
-        virtual void* v_resize(ntlsf::size_t) final;
-
-        virtual void* v_allocate(u32 size, u32 alignment) final;
-        virtual void  v_deallocate(void* ptr) final;
+        virtual void* v_resize(u64 size);
 
         DCORE_CLASS_PLACEMENT_NEW_DELETE
     };
 
-    tlsf_alloc_t::tlsf_alloc_t() : mMemory(nullptr), mMemorySize(0) {}
-    tlsf_alloc_t::~tlsf_alloc_t() {}
+    tlsf_alloc_simple_t::tlsf_alloc_simple_t() : mMemory(nullptr), mMemorySize(0) {}
+    tlsf_alloc_simple_t::~tlsf_alloc_simple_t() {}
 
-    void* tlsf_alloc_t::v_resize(ntlsf::size_t size)
+    void* tlsf_alloc_simple_t::v_resize(u64 size)
     {
         if (size > mMemorySize)
             return nullptr;
         return mMemory;
     }
 
-    void* tlsf_alloc_t::v_allocate(u32 size, u32 alignment)
-    {
-        if (alignment < 16)
-            return malloc(size);
-        return aalloc(alignment, size);
-    }
-
-    void tlsf_alloc_t::v_deallocate(void* ptr) { free(ptr); }
-
     alloc_t* g_create_tlsf(void* mem, int_t mem_size)
     {
-        tlsf_alloc_t* alloc = new (mem) tlsf_alloc_t();
-        alloc->mMemory      = nmem::ptr_align( (u8*)mem + sizeof(tlsf_alloc_t), 16);
-        alloc->mMemorySize  = nmem::ptr_diff(alloc->mMemory, mem);
-        alloc->setup();
+        tlsf_alloc_simple_t* alloc = new (mem) tlsf_alloc_simple_t();
+        alloc->mMemory             = nmem::ptr_align((u8*)mem + sizeof(tlsf_alloc_simple_t), 16);
+        alloc->mMemorySize         = nmem::ptr_diff(alloc->mMemory, mem);
+        ntlsf::tlsf_setup(alloc);
         return alloc;
     }
 
--- source/main/include/callocator/c_allocator_tlsf.h
+++ source/main/include/callocator/c_allocator_tlsf.h
@@ -9,7 +9,48 @@
 
 namespace ncore
 {
+    namespace ntlsf
+    {
+        struct tlsf_block_t;
+
+        enum ELevelCounts
+        {
+#if __SIZE_WIDTH__ == 64
+            TLSF_SL_COUNT = 16,
+            TLSF_FL_COUNT = 32,
+            TLSF_FL_MAX   = 38,
+#else
+            TLSF_SL_COUNT = 16,
+            TLSF_FL_COUNT = 25,
+            TLSF_FL_MAX   = 30,
+#endif
+        };
+    } // namespace ntlsf
+
+    class tlsf_alloc_t : public alloc_t
+    {
+    public:
+        u32                  fl, sl[ntlsf::TLSF_FL_COUNT];
+        ntlsf::tlsf_block_t* block[ntlsf::TLSF_FL_COUNT][ntlsf::TLSF_SL_COUNT];
+        u64                  size;
+
+        tlsf_alloc_t();
+        virtual ~tlsf_alloc_t();
+
+        virtual void* v_allocate(u32 size, u32 alignment) final;
+        virtual void  v_deallocate(void* ptr) final;
+
+        virtual bool  v_check(const char*& error_description) const;
+        virtual void* v_resize(u64 size) = 0;
+
+        DCORE_CLASS_PLACEMENT_NEW_DELETE
+    };
+
+    // Create a TLSF allocator that is backed by a single fixed memory block.
     alloc_t* g_create_tlsf(void* mem, int_t mem_size);
+
+    // Note: It is very straightforward to implement a TLSF allocator that is backed by a
+    // virtual memory system. This would allow the allocator to grow and shrink as needed.
 
 }; // namespace ncore
 
