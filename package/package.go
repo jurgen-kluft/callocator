b496de75185c39c5c92166d79dc420591bc7c946
jurgen.kluft <jurgen.kluft@gmail.com>
2025-06-01 08:32:21 +0800

    * adjust to changes in ccode

--- package/package.go
+++ package/package.go
@@ -6,27 +6,39 @@ import (
 	cunittest "github.com/jurgen-kluft/cunittest/package"
 )
 
-// GetPackage returns the package object of 'callocator'
+const (
+	repo_path = "github.com\\jurgen-kluft\\"
+	repo_name = "callocator"
+)
+
 func GetPackage() *denv.Package {
-	// Dependencies
+	name := repo_name
+
+	// dependencies
 	cunittestpkg := cunittest.GetPackage()
 	cbasepkg := cbase.GetPackage()
 
-	// The main (callocator) package
-	mainpkg := denv.NewPackage("callocator")
+	// main package
+	mainpkg := denv.NewPackage(repo_path, repo_name)
 	mainpkg.AddPackage(cunittestpkg)
 	mainpkg.AddPackage(cbasepkg)
 
-	// 'callocator' library
-	mainlib := denv.SetupCppLibProject("callocator", "github.com\\jurgen-kluft\\callocator")
+	// main library
+	mainlib := denv.SetupCppLibProject(mainpkg, name)
 	mainlib.AddDependencies(cbasepkg.GetMainLib()...)
 
-	// 'callocator' unittest project
-	maintest := denv.SetupDefaultCppTestProject("callocator"+"_test", "github.com\\jurgen-kluft\\callocator")
+	// test library
+	testlib := denv.SetupCppTestLibProject(mainpkg, name)
+	testlib.AddDependencies(cbasepkg.GetTestLib()...)
+	testlib.AddDependencies(cunittestpkg.GetTestLib()...)
+
+	// unittest project
+	maintest := denv.SetupCppTestProject(mainpkg, name)
 	maintest.AddDependencies(cunittestpkg.GetMainLib()...)
-	maintest.Dependencies = append(maintest.Dependencies, mainlib)
+	maintest.AddDependency(testlib)
 
 	mainpkg.AddMainLib(mainlib)
+	mainpkg.AddTestLib(testlib)
 	mainpkg.AddUnittest(maintest)
 	return mainpkg
 }
